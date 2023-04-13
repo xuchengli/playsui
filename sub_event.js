1a72b20289b43d5bffeb744d0f881f54bcce5942
xuchengli <lixucheng@aliyun.com>
2023-04-13 14:39:23 +0800

    debug event subscription

--- sub_event.js
+++ sub_event.js
@@ -1,18 +1,43 @@
 const {
     JsonRpcProvider,
     // localnetConnection,
+    devnetConnection,
     Connection,
 } = require('@mysten/sui.js');
 const { packageObjectId } = require('./config');
 
 // const provider = new JsonRpcProvider(localnetConnection);
 
-const connection = new Connection({
-    fullnode: 'http://39.107.236.62:9000',
-});
-const provider = new JsonRpcProvider(connection);
+// const connection = new Connection({
+//     fullnode: 'http://39.107.236.62:9000',
+// });
+// const provider = new JsonRpcProvider(connection);
+
+const provider = new JsonRpcProvider(devnetConnection);
+
+const subEvent = async (pkg, module) => {
+    console.log('startup subscribe...');
+
+    const subscriptionId = await provider.subscribeEvent({
+        filter: { MoveModule: { package: pkg, module} },
+        onMessage(event) {
+            console.log(event);
+        },
+    });
+    console.log('startup subscribe...', subscriptionId);
+
+    setTimeout(async (pkg, module) => {
+        if (subscriptionId) {
+            console.log('remove subscription>>>>>', await provider.unsubscribeEvent({ id: subscriptionId }));
+        }
+        await subEvent(pkg, module);
+    }, 20000, pkg, module);
+}
 
 (async () => {
+    await subEvent(packageObjectId, 'counter');
+
+    /*
     const subscriptionId = await provider.subscribeEvent({
         filter: { MoveModule: { package: packageObjectId, module: 'counter'} },
         onMessage(event) {
@@ -20,4 +45,5 @@ const provider = new JsonRpcProvider(connection);
         },
     });
     console.log(subscriptionId);
+    */
 })();
