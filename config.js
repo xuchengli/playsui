eaa45fb6a89b3de486bd0466b202c78afa1f3bcf
xuchengli <lixucheng@aliyun.com>
2023-04-13 11:40:39 +0800

    change rpc server

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    packageObjectId: '0x89d35014c317d5785536f5ef6d0ce722aae4d3f4fa8e4825b8257737268d8c1b',
-    counterObjectId: '0x0443ef556bdef7e7ff8bc822e6e9e1d85e7bef826547e8d65f3de9245ea554d0',
+    packageObjectId: '0x96aa6ddf66069f6316ada3548a5e840d681384dc573c48239c5d139a68dded16',
+    counterObjectId: '0x3f0e88f60a249526dbc8900a207dc2a5253a8fe43f79e00da8f16340b3c11d1c',
 };
--- read.js
+++ read.js
@@ -1,10 +1,16 @@
 const {
     JsonRpcProvider,
-    localnetConnection,
+    // localnetConnection,
+    Connection,
 } = require('@mysten/sui.js');
 const { packageObjectId } = require('./config');
 
-const provider = new JsonRpcProvider(localnetConnection);
+// const provider = new JsonRpcProvider(localnetConnection);
+
+const connection = new Connection({
+    fullnode: 'http://39.107.236.62:9000',
+});
+const provider = new JsonRpcProvider(connection);
 
 (async () => {
     const txn = await provider.getObject({
