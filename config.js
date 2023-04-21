1bb41edc647ba3db763a62cb2bc3c546be5b530b
xuchengli <lixucheng@aliyun.com>
2023-04-21 11:53:51 +0800

    try to query multiple events

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    packageObjectId: '0x96aa6ddf66069f6316ada3548a5e840d681384dc573c48239c5d139a68dded16',
-    counterObjectId: '0x3f0e88f60a249526dbc8900a207dc2a5253a8fe43f79e00da8f16340b3c11d1c',
+    packageObjectId: '0x9d12400e7857afd7b7c7f2001680ca291c1b786e4cce77372fcaea5bf84bdf1d',
+    // packageObjectId: '0x3e521d6829ec4ef5c65a47b66635947892d4145f8bc8f598d779633cb8564cf0',
+    counterObjectId: '0xef03dea26b7bb8aef5a4efba7c88a24d2a33a05be13cb684773d84333d2f40ae',
+    // counterObjectId: '0xab0ffa0403c637aa963a8d8a1d56acae1eb28dc1ce7dc84f21c28d3c7b1e222b',
     clientAddress: '0xe42d79acbbc17c214518bad150b0e6d47c111c2796b4069520b15158a3915217',
 };
--- read.js
+++ read.js
@@ -15,7 +15,30 @@ const { packageObjectId, clientAddress } = require('./config');
 
 const provider = new JsonRpcProvider(devnetConnection);
 
+function sleep(n) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n * 1000);
+}
+
+async function querySuiEvents(moveModule, cursor = {}) {
+
+    const filter = {
+        query: { MoveModule: { ...moveModule } },
+        limit: 10,
+        order: 'ascending',
+    };
+
+    // 判断是否需要翻页
+    if (Object.keys(cursor).length !== 0) {
+        Object.assign(filter, { cursor });
+    }
+
+    // console.log('查询条件:', filter);
+
+    return await provider.queryEvents(filter);
+}
+
 (async () => {
+    /*
     const txn = await provider.getObject({
         id: packageObjectId,
         options: {
@@ -23,10 +46,35 @@ const provider = new JsonRpcProvider(devnetConnection);
         },
     });
     console.log(txn);
+    */
 
-    const events = await provider.queryEvents({
-        query: { Sender: clientAddress },
-        limit: 2,
-    });
-    console.log(events);
+    let moveModules = [
+        {
+            package: '0x9d12400e7857afd7b7c7f2001680ca291c1b786e4cce77372fcaea5bf84bdf1d',
+            module: 'counter',
+            cursor: {},
+        },
+        {
+            package: '0x3e521d6829ec4ef5c65a47b66635947892d4145f8bc8f598d779633cb8564cf0',
+            module: 'counter',
+            cursor: {},
+        },
+    ];
+    setInterval(async() => {
+        try {
+            const querys = moveModules.map(async moveModule => {
+                const { package, module, cursor } = moveModule;
+                return await querySuiEvents({ package, module }, cursor);
+            });
+            const events = await Promise.all(querys);
+
+            for ([i, event] of events.entries()) {
+                console.log(event.data);
+
+                moveModules[i].cursor = event.nextCursor;
+            }
+        } catch (err) {
+            console.err(err);
+        }
+    }, 10000);
 })();
